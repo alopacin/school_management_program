78b5b009d6255f4079e5f5d33897f5f78754998c
alopacin <adlopacinski@gmail.com>
2023-08-27 15:58:50 +0200

    zapisanie pracy

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from classes.teacher import Teacher
 from classes.educator import Educator
 
 #tworzenie slownikow
-student_list = {}
-teacher_list = {}
-educator_list = {}
+student_list = []
+teacher_list = []
+educator_list = []
 
 conditions = ('utwórz', 'utworz', 'zarządzaj', 'zarzadzaj', 'koniec')
 while True:
-    print(student_list)
+    for student in student_list:
+        print(student)
+    print(educator_list)
     ask = input('Co chcesz zrobić (utwórz, zarządzaj, koniec)?: ')
     if ask not in conditions:
         print('Podałeś nieprawidłową komendę.Spróbuj jeszcze raz')
@@ -26,26 +28,28 @@ while True:
         elif inquiry == 'uczeń' or inquiry == 'uczen':
             pupil_name = input('Podaj imię i nazwisko ucznia: ')
             pupil_class = input('Podaj do jakiej klasy chodzi uczeń: ')
-            new_student = Pupil(pupil_name)
-            student_list[new_student] = pupil_class
+            new_student = Pupil(pupil_name, pupil_class)
+            student_list.append(new_student)
         elif inquiry == 'nauczyciel':
             teacher_name = input('Podaj imię i nazwisko nauczyciela: ')
             teacher_subject = input('Podaj nazwę przedmiotu, który prowadzi nauczyciel: ')
-            teacher_class = input('Podaj klasę/y, które prowadzi nauczyciel: ')
-            new_teacher = Teacher(teacher_name)
-            teacher_list[new_teacher] = teacher_subject, teacher_class
+            while True:
+                teacher_class = input('Podaj klasę/y, które prowadzi nauczyciel: ')
+                if teacher_class == '':
+                    break
+                new_teacher = Teacher(teacher_name, teacher_subject, teacher_class)
         elif inquiry == 'wychowawca':
             educator_name = input('Podaj imię i nazwisko wychowawcy: ')
             educator_class = input('Podaj klasę jaką prowadzi wychowawca: ')
-            new_educator = Educator(educator_name)
-            educator_list[new_educator] = educator_class
+            new_educator = Educator(educator_name, educator_class)
+            educator_list.append(new_educator)
         elif inquiry == 'koniec':
             break
 
 #czesc programu odpowiadajaca za zarzadzanie uzytkownikami
     elif ask == 'zarządzaj' or ask == 'zarzadzaj':
         conditionsss = ['klasa', 'uczeń', 'uczen', 'nauczyciel', 'wychowawca']
-        inquiry = input('Wybierz następującą opcję: \nklasa\nuczeń\nnauczyciel\nwychowawca\nkoniec')
+        inquiry = input('Wybierz następującą opcję: \nklasa\nuczeń\nnauczyciel\nwychowawca\nkoniec\n')
         if inquiry not in conditionsss :
             print('Spróbuj jeszcze raz')
             break
